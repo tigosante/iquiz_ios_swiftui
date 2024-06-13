7a1dd45240589fbbd6fe1f25849c1644501edaa2
Tiago Silva <tsilvasantos38@gmail.com>
2024-06-13 09:53:38 -0300

    Initial Commit

---
+++ iquiz/iquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iquizApp.swift
+//  iquiz
+//
+//  Created by Tiago Silva on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct iquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
